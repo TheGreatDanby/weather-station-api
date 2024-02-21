418434e47361329e701e36354cc2a4352f7a4928
Daniel <100142606+TheGreatDanby@users.noreply.github.com>
2024-02-21 23:32:06 +0800

    updated

--- database/mdb.js
+++ database/mdb.js
@@ -1,12 +1,6 @@
-import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb";
 
-const connectionString = "mongodb+srv://weather_api_db:jHVnf7A8WDtPytR8@weatherdata.pfr7etc.mongodb.net/test?proxyHost=mongodb.bypass.host&proxyPort=80&proxyUsername=student&proxyPassword=student"
+const connectionString = process.env.MONGODB;
 
-const client = new MongoClient(connectionString)
-export const db = client.db("weatherData")
-
-
-
-// mongodb+srv://thegreatdanby_weather:aycfcTA6Q1SYWyys_weather@weatherdata.pfr7etc.mongodb.net/test?proxyHost=168.138.109.243&proxyPort=80&proxyUsername=student&proxyPassword=student
-// "mongodb+srv://thegreatdanby_weather:aycfcTA6Q1SYWyys_weather@weatherdata.pfr7etc.mongodb.net/test?proxyHost=mongodb.bypass.host&proxyPort=80&proxyUsername=student&proxyPassword=student"
-// "mongodb+srv://thegreatdanby_weather:aycfcTA6Q1SYWyys_weather@weatherdata.pfr7etc.mongodb.net/test"
+const client = new MongoClient(connectionString);
+export const db = client.db("weatherData");
